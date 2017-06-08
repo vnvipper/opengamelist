bd54aff55110c3087bd51b0bdd6a41cd955dd556
nguyenpv113 <nguyenpvdtu@gmail.com>
2017-06-08 17:12:45 +0700

    initial

---
+++ calledOpenGameListWebApp/Controllers/ItemsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using calledOpenGameListWebApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace calledOpenGameListWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class ItemsController : Controller
+    {        // GET api/items/GetLatest/5        
+
+        [HttpGet("GetLatest/{num}")]
+        public JsonResult GetLatest(int num)
+        {
+            var arr = new List<ItemViewModel>();
+            for (int i = 1; i <= num; i++)
+                arr.Add(new ItemViewModel()
+                {
+                    Id = i,
+                    Title = $"Item {i} Title",
+                    Description = $"Item {i} Description"
+                });
+            var settings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented
+            };
+
+            return new JsonResult(arr, settings);
+        }
+    }
+}
